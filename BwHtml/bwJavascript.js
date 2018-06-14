2a9205dedbf6bfa43397da9bf2f237d62f680bd6
Sameet <sameetnaik@gmail.com>
2018-06-14 16:47:09 +0200

    Minor fixes

---
+++ BwHtml/bwJavascript.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function modify_qty(val) {
+    var qty = document.getElementById('qty').value;
+    var new_qty = parseInt(qty,10) + val;
+    
+    if (new_qty < 0) {
+        new_qty = 0;
+    }
+    
+    document.getElementById('qty').value = new_qty;
+    return new_qty;
+}
