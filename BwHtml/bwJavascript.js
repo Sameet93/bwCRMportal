fb1e0ab76602540e611776487f7e153d18e77fa9
Sameet <sameetnaik@gmail.com>
2018-07-25 12:44:20 +0200

    Look and feel

--- BwHtml/bwJavascript.js
+++ BwHtml/bwJavascript.js
@@ -30,5 +30,26 @@ function validatePassword() {
     }
 }
 
+$(function() {
+ 
+    // if the function argument is given to overlay,
+    // it is assumed to be the onBeforeLoad event listener
+    $("a[rel]").overlay({
+ 
+        mask: 'darkred',
+        effect: 'apple',
+ 
+        onBeforeLoad: function() {
+ 
+            // grab wrapper element inside content
+            var wrap = this.getOverlay().find(".contentWrap");
+ 
+            // load the page specified in the trigger
+            wrap.load(this.getTrigger().attr("href"));
+        }
+ 
+    });
+});
+
 pword.onchange = validatePassword;
 cpword.onkeyup = validatePassword;
