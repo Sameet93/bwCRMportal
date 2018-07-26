6cce00d2e950ea0b8ef4bf9cf21b18a9ac1bbf96
Sameet <sameetnaik@gmail.com>
2018-07-26 11:57:16 +0200

    Js

--- BwHtml/bwJavascript.js
+++ BwHtml/bwJavascript.js
@@ -30,26 +30,69 @@ function validatePassword() {
     }
 }
 
-$(function() {
- 
+$(function () {
+
     // if the function argument is given to overlay,
     // it is assumed to be the onBeforeLoad event listener
     $("a[rel]").overlay({
- 
+
         mask: 'darkred',
         effect: 'apple',
- 
-        onBeforeLoad: function() {
- 
+
+        onBeforeLoad: function () {
+
             // grab wrapper element inside content
             var wrap = this.getOverlay().find(".contentWrap");
- 
+
             // load the page specified in the trigger
             wrap.load(this.getTrigger().attr("href"));
         }
- 
+
     });
 });
 
 pword.onchange = validatePassword;
 cpword.onkeyup = validatePassword;
+
+// Test Code
+
+$(function () {
+
+    var newHash = "",
+        $mainContent = $("#main-content"),
+        $pageWrap = $("#page-wrap"),
+        baseHeight = 0,
+        $el;
+
+    $pageWrap.height($pageWrap.height());
+    baseHeight = $pageWrap.height() - $mainContent.height();
+
+    $("nav").delegate("a", "click", function () {
+        window.location.hash = $(this).attr("href");
+        return false;
+    });
+
+    $(window).bind('hashchange', function () {
+
+        newHash = window.location.hash.substring(1);
+
+        if (newHash) {
+            $mainContent
+                .find("#guts")
+                .fadeOut(200, function () {
+                    $mainContent.hide().load(newHash + " #guts", function () {
+                        $mainContent.fadeIn(200, function () {
+                            $pageWrap.animate({
+                                height: baseHeight + $mainContent.height() + "px"
+                            });
+                        });
+                        $("nav a").removeClass("current");
+                        $("nav a[href='" + newHash + "']").addClass("current");
+                    });
+                });
+        };
+
+    });
+
+    $(window).trigger('hashchange');
+});
