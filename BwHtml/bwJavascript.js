b8b6d0767166b31c4fbf40a2b484c02b4490551b
AlenzoM <AlenzoM@users.noreply.github.com>
2018-08-13 19:05:28 +0200

    Help Page ( incomplete); Dashboard(incomplete);

--- BwHtml/bwJavascript.js
+++ BwHtml/bwJavascript.js
@@ -17,6 +17,15 @@ function closeNav() {
 
 }
 
+//Panel active when opened
+$('.panel-heading a').click(function() {
+    $('.panel-heading').removeClass('active');
+    
+    //If the panel was open and would be closed by this click, do not active it
+    if(!$(this).closest('.panel').find('.panel-collapse').hasClass('in'))
+        $(this).parents('.panel-heading').addClass('active');
+ });
+
 
 function modify_qty(val) {
     var qty = document.getElementById('qty').value;
