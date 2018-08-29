4ac3451f137190e5ab12f0e991cad3bdd9262099
AlenzoM <AlenzoM@users.noreply.github.com>
2018-08-29 16:24:05 +0200

    New My Account; and Help Page

--- BwHtml/bwJavascript.js
+++ BwHtml/bwJavascript.js
@@ -1,11 +1,5 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+
+
 
 //Sidenav open and close
 function openNav() {
@@ -17,14 +11,32 @@ function closeNav() {
 
 }
 
+//FAQ Search
+function searchFilter() {
+    var input, filter, ol, li,  a, i;
+    input = document.getElementById("searchbar");
+    filter = input.value.toUpperCase();
+    ol = document.getElementById("faqlist");
+    li = ol.getElementsByClassName("faq_question");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
 //Panel active when opened
-$('.panel-heading a').click(function() {
+/*$('.panel-heading a').click(function() {
     $('.panel-heading').removeClass('active');
     
     //If the panel was open and would be closed by this click, do not active it
     if(!$(this).closest('.panel').find('.panel-collapse').hasClass('in'))
         $(this).parents('.panel-heading').addClass('active');
- });
+ });*/
 
 
 function modify_qty(val) {
